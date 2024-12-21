4c856de7c0107c9d7a7ac3efa9b96593622f0550
masungil <masungil70@gmail.com>
2024-12-21 14:56:26 +0900

    2번째 파일 추가

---
+++ src/main/java/kr/ac/koreatech/devops_step0/HelloController.java
@@ -0,0 +1,19 @@
+package kr.ac.koreatech.devops_step0;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Controller
+@Slf4j
+public class HelloController {
+  @GetMapping("/")
+  public String index() {
+      log.info("안녕하세요. 서버에서 실행됨");
+      return "index.html";
+  }
+  
+}
