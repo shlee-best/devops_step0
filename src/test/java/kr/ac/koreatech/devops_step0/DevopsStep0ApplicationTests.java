cdf648fb6d06cbe04606b18323f9d2bf36f0e313
masungil <masungil70@gmail.com>
2024-12-22 10:21:31 +0900

    test 실패 예제제

--- src/test/java/kr/ac/koreatech/devops_step0/DevopsStep0ApplicationTests.java
+++ src/test/java/kr/ac/koreatech/devops_step0/DevopsStep0ApplicationTests.java
@@ -1,5 +1,7 @@
 package kr.ac.koreatech.devops_step0;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,22 @@ class DevopsStep0ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	int add(int a, int b) {
+		return a + b;
+	}
+
+	@Test
+	void add_test1() {
+		int result = add(10, 20);
+		assertEquals(30, result);
+	}
+
+	@Test
+	void add_test2() {
+		int result = add(10, 20);
+		assertEquals(10, result);
 	}
 
 }
